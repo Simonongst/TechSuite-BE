210946ecb0eaef8e0bb077c8a8147278abe8f794
Simon Ong <simonongst@gmail.com>
2025-12-04 23:31:48 +0800

    feat: add Currency mongoose schema

---
+++ models/currency.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const currencySchema = new mongoose.Schema(
+  {
+    code: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    label: { 
+        type: String, 
+        required: true 
+    },
+    rateToBase: { 
+        type: Number, 
+        required: true 
+    },
+    isActive: { 
+        type: Boolean, 
+        default: true 
+    },
+  },
+  { timestamps: true }
+);
+
+const Currency = mongoose.model('Currency', currencySchema);
+
+module.exports = Currency;
