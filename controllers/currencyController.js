f0a91d30b6f076c2ee72da403feda54b662bae93
Simon Ong <simonongst@gmail.com>
2025-12-05 00:11:14 +0800

    feat: add createCurrency controller and POST /currencies route

--- controllers/currencyController.js
+++ controllers/currencyController.js
@@ -9,6 +9,38 @@ const getAllCurrency = async (req, res) => {
   }
 };
 
+const createCurrency = async (req, res) => {
+  try {
+    const { code, ...currencyData } = req.body;
+
+    if (!code) {
+      return res.status(400).json({
+        success: false,
+        message: 'Currency code is required.',
+      });
+    }
+
+    const codeExists = await Currency.findOne({ code });
+    if (codeExists) {
+      return res.json({
+        success: false,
+        message: 'Code already exists.',
+      });
+    }
+
+    const newCurrency = new Currency({ code, ...currencyData });
+    const savedCurrency = await newCurrency.save();
+
+    res.status(201).json({ success: true, data: savedCurrency });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+};
+
 module.exports = {
-    getAllCurrency,
-}
+  getAllCurrency,
+  createCurrency,
+};
--- routers/currencyRoutes.js
+++ routers/currencyRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const currencyController = require('../controllers/currencyController');
 
-const { getAllCurrency } = currencyController;
+const { getAllCurrency, createCurrency } = currencyController;
 
 router.get('/', getAllCurrency);
+router.post('/', createCurrency);
 
 module.exports = router;
