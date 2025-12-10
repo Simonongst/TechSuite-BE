c7e63486f3eaca18644efd1629ae04ef743c76d1
Simon Ong <simonongst@gmail.com>
2025-12-10 23:33:14 +0800

    feat: add createUser endpoint with validation and password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.js');
+const bcrypt = require('bcrypt');
 
 const getAllUsers = async (req, res) => {
   try {
@@ -18,7 +19,49 @@ const getUserById = async (req, res) => {
   }
 };
 
+const createUser = async (req, res) => {
+  try {
+    const { eid, email, ...newUser } = req.body;
+
+    if (eid) {
+      const eidExists = await User.findOne({ eid });
+      if (eidExists) {
+        return res.json({
+          success: false,
+          message: 'EID already exists.',
+        });
+      }
+    }
+
+    if (email) {
+      const emailExists = await User.findOne({ email });
+      if (emailExists) {
+        return res.json({
+          success: false,
+          message: 'Email already exists.',
+        });
+      }
+    }
+
+    const userToSave = new User({ eid, email, ...newUser });
+
+    const hashedPassword = bcrypt.hashSync(userToSave.password, 10);
+    userToSave.password = hashedPassword;
+
+    const savedUser = await User.create(userToSave);
+    const { password, ...userWithoutPassword } = savedUser.toObject();
+
+    res.status(201).json({ success: true, data: userWithoutPassword });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+};
+
 module.exports = {
   getAllUsers,
   getUserById,
+  createUser,
 };
