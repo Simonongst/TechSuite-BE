c57276595fbe56d768098622a7d1711f51b3620c
Simon Ong <simonongst@gmail.com>
2025-12-12 00:54:59 +0800

    feat: register POST for /sign-out, /change-password with verifyToken, /forgot-password and /reset-password

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, signIn } = require('../controllers/authController.js');
+const {
+  signUp,
+  signIn,
+  signOut,
+  changePassword,
+  forgotPassword,
+  resetPassword,
+} = require('../controllers/authController.js');
+const verifyToken = require('../middleware/verifyToken');
 
 router.post('/sign-up', signUp);
-router.post('/sign-up', signIn);
+router.post('/sign-in', signIn);
+router.post('/sign-out', signOut);
+router.post('/change-password', verifyToken, changePassword);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
